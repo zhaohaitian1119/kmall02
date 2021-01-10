e732a02dd63153864b4fc08ae631bb3cab0224b0
zhaohaitian_glb <397408662@qq.com>
2021-01-10 14:36:15 +0800

    购物车模块显示

---
+++ kmallserviceutils/src/main/java/com/kgc/kmall01/config/RedisConfing.java
@@ -0,0 +1,31 @@
+package com.kgc.kmall01.config;
+
+import com.kgc.kmall01.utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shkstart
+ * @create 2021-01-04 14:32
+ */
+@Configuration
+public class RedisConfing {
+
+    @Value("${spring.redis.host:disabled}")
+    private String host;
+    @Value("${spring.redis.port:6379}")
+    private Integer port;
+    @Value("${spring.redis.database:0}")
+    private Integer database;
+
+    @Bean
+    public RedisUtils getRedisUtils(){
+        if(host.equals("disabled")){
+            return null;
+        }
+        RedisUtils redisUtils = new RedisUtils();
+        redisUtils.initPool(host,port,database);
+        return redisUtils;
+    }
+}
